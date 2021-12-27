7174441a3d706261b033260699d2c0087819e423
Rafael <rafaelplaurindo@gmail.com>
2021-12-27 18:27:46 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 export { Autocomplete } from './lib/autocomplete.js';
 export { RecursivePromise } from './lib/recursive-promise.js';
-export { Search } from './lib/search.js';
 export { Structure } from './lib/structure.js';
 export { StrategyContext } from './lib/patterns/gof/strategy-context.js';
 
