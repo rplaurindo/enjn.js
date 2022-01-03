3db360fee4bab1f3a62d12e24f6547013a7e30b2
Rafael <rafaelplaurindo@gmail.com>
2022-01-03 14:50:53 -0300

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// imports TODO
-// import './utils/autocomplete.js';
-import './lib/extensions/index.js';
-
-import './enjn.js';
-
-// a namespace for all modules
-import * as enJn from './api.js';
-export { enJn };
